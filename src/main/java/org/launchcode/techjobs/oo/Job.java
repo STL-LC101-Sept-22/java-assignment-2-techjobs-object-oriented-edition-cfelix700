6f7719cf3ff1265a9fef6838fb6ee69b0d163c8e
cfelix700 <cfelix700@gmail.com>
2023-01-31 19:31:42 -0600

    Steps 1, 2, and 3 done(i think)!

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -94,4 +94,7 @@ public class Job {
     public int getId() {
         return id;
     }
+
+    public void setId(int i) {
+    }
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,50 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.CoreCompetency;
+import org.launchcode.techjobs.oo.Employer;
+import org.launchcode.techjobs.oo.Location;
+import org.launchcode.techjobs.oo.PositionType;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job.getName().equals("Product tester"));
+        assertTrue(job.getEmployer().getValue().equals("ACME"));
+        assertTrue(job.getLocation().getValue().equals("Desert"));
+        assertTrue(job.getPositionType().getValue().equals("Quality control"));
+        assertTrue(job.getCoreCompetency().getValue().equals("Persistence"));
+
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert") , new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert") , new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+
+        job1.setId(1);
+        job2.setId(2);
+
+        assertFalse(job1.equals(job2));
+    }
 
 }
+
+
